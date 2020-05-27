3fec956876908dab3a14cad5a022036a1701fff5
masterambi <ramzyrash@gmail.com>
2020-05-27 16:12:33 +0700

    Navigation bars setup

--- TwitterClone/Controller/ExploreController.swift
+++ TwitterClone/Controller/ExploreController.swift
@@ -17,9 +17,13 @@ class ExploreController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBlue
+        configureUI()
     }
     
     // MARK: - Helpers
 
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Explore"
+    }
 }
