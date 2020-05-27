cc5148b027b55d1715201e36b09846986d543ef8
masterambi <ramzyrash@gmail.com>
2020-05-27 12:55:54 +0700

    Initial project setup

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by Ramzy Rashaun Arief on 27/05/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
