8fbce5d7ab9bad5ba37374708739fdc08ca5329d
masterambi <ramzyrash@gmail.com>
2020-05-29 19:10:03 +0700

    Configure upload user data and profile image while authenticating the user

---
+++ TwitterClone/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TwitterClone
+//
+//  Created by Ramzy Rashaun Arief on 29/05/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
