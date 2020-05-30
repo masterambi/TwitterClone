52eba284fcea4f844b7b9cba31e7fce3833057cb
masterambi <ramzyrash@gmail.com>
2020-05-30 12:52:10 +0700

    Configure SDWebImage install and passing user object to feedcontroller

--- TwitterClone/API/UserService.swift
+++ TwitterClone/API/UserService.swift
@@ -11,14 +11,14 @@ import Firebase
 struct UserService {
     static let shared = UserService()
     
-    func fetchUser() {
+    func fetchUser(completion: @escaping(User) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         REF_USERS.child(uid).observeSingleEvent(of: .value) { snapshot in
             guard let dictionary = snapshot.value as? [String: AnyObject] else { return }
-            let user = User(uid: uid, dictionary: dictionary)
             
-            print("DEBUG: Username is \(user.username)")
+            let user = User(uid: uid, dictionary: dictionary)
+            completion(user)
         }
     }
 }
