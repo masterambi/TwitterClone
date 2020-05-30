15866f751ab07457c632f84ea322c2736945860c
masterambi <ramzyrash@gmail.com>
2020-05-30 14:03:04 +0700

    Configure load user profile image

--- TwitterClone/Controller/FeedController.swift
+++ TwitterClone/Controller/FeedController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FeedController: UIViewController {
 
     // MARK: - Properties
     
     var user: User? {
-        didSet { print("DEBUG: Did set user in feed controller...") }
+        didSet { configureLeftBarButton() }
     }
     
     // MARK: - Lifecycle
@@ -28,16 +29,26 @@ class FeedController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: nil)
+        navigationItem.rightBarButtonItem?.tintColor = .twitterBlue
+        
         let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
-        
+    }
+    
+    func configureLeftBarButton() {
+        guard let user = user else { return }
         let profileImageView = UIImageView()
         profileImageView.backgroundColor = .twitterBlue
         profileImageView.setDimensions(width: 32, height: 32)
         profileImageView.layer.cornerRadius = 32 / 2
+        profileImageView.layer.masksToBounds = true
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
+        
     }
 
 }
