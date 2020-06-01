72dc53714206537f94041ab014944746c6709934
masterambi <ramzyrash@gmail.com>
2020-06-01 16:02:51 +0700

    Configure showing ProfileController

---
+++ TwitterClone/Controller/ProfileController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileController.swift
+//  TwitterClone
+//
+//  Created by Ramzy Rashaun Arief on 01/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ProfileController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .white
+    }
+
+}
