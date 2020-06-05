9520e32edb553fc9060db3879c5f4d58f752228d
masterambi <ramzyrash@gmail.com>
2020-06-05 15:13:36 +0700

    Configure UploadTweetViewModel

---
+++ TwitterClone/ViewModel/UploadTweetViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UploadTweetViewModel.swift
+//  TwitterClone
+//
+//  Created by Ramzy Rashaun Arief on 05/06/20.
+//  Copyright © 2020 Ramzy Rashaun Arief. All rights reserved.
+//
+
+import UIKit
+
+enum UploadTweetConfiguration {
+    case tweet
+    case reply(Tweet)
+}
+
+struct UploadTweetViewModel {
+    let actionButtonTitle: String
+    let placeholderText: String
+    var shouldShowReplyLabel: Bool
+    var replyText: String?
+    
+    init(config: UploadTweetConfiguration) {
+        switch config {
+        case .tweet:
+            actionButtonTitle = "Tweet"
+            placeholderText = "What's happening"
+            shouldShowReplyLabel = false
+        case .reply(let tweet):
+            actionButtonTitle = "Reply"
+            placeholderText = "Tweet your reply"
+            shouldShowReplyLabel = true
+            replyText = "Replying to @\(tweet.user.username)"
+        }
+    }
+}
