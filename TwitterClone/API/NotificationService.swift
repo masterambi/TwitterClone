8912d7218e38a8e6d3ef941dafff7b787c301413
masterambi <ramzyrash@gmail.com>
2020-06-08 13:54:25 +0700

    Configure upload follow and reply notification

--- TwitterClone/API/NotificationService.swift
+++ TwitterClone/API/NotificationService.swift
@@ -11,7 +11,9 @@ import Firebase
 struct NotificationService {
     static let shared = NotificationService()
     
-    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+    func uploadNotification(type: NotificationType,
+                            tweet: Tweet? = nil,
+                            user: User? = nil) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
@@ -21,8 +23,8 @@ struct NotificationService {
         if let tweet = tweet {
             values["tweetID"] = tweet.tweetID
             REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
-        } else {
-            
+        } else if let user = user {
+            REF_NOTIFICATIONS.child(user.uid).childByAutoId().updateChildValues(values)
         }
     }
 }
--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -84,6 +84,10 @@ class UploadTweetController: UIViewController {
                 return
             }
             
+            if case .reply(let tweet) = self.config {
+                NotificationService.shared.uploadNotification(type: .reply, tweet: tweet)
+            }
+            
             self.dismiss(animated: true, completion: nil)
         }
     }
